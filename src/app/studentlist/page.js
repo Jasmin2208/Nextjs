fefcbc186ade1c7a27ab464b91baece6a864d966
Jasmin2208 <jasminkorat204@gmail.com>
2023-08-28 17:51:48 +0530

    Make Array Of Different Name Path For Dynamic Routing

--- src/app/studentlist/page.js
+++ src/app/studentlist/page.js
@@ -2,25 +2,19 @@ import Link from 'next/link'
 import React from 'react'
 
 function StudentList() {
-  return (
-    <div>
-        <h1>Student List</h1>
-        <ul>
-            <li>
-                <Link href="/studentlist/jasmin">Jasmin</Link>
-            </li>
-            <li>
-                <Link href="/studentlist/ajay">Ajay</Link>
-            </li>
-            <li>
-                <Link href="/studentlist/happy">Happy</Link>
-            </li>
-            <li>
-                <Link href="/studentlist/Vraj">Vraj</Link>
-            </li>
-        </ul>
-    </div>
-  )
+    const studentsName = ["jasmin", "ajay", "vraj", "happy"]
+    return (
+        <div>
+            <h1>Student List</h1>
+            <ul>
+                {
+                    studentsName.map((student) => {
+                        return <li><Link href={`/studentlist/${student}`}> {student} </Link></li>
+                    })
+                }
+            </ul>
+        </div>
+    )
 }
 
 export default StudentList
