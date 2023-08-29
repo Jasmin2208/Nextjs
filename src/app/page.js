ff3f698e5b67c5fa4b351a1543cebd7794f2b3d6
Jasmin2208 <jasminkorat204@gmail.com>
2023-08-29 17:13:15 +0530

    Conditional style and CSS

--- src/app/page.js
+++ src/app/page.js
@@ -3,24 +3,25 @@ import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
 import custom from './custom.module.css'
-import other from  './other.module.css'
+import other from './other.module.css'
 import outside from './style/outside.module.css'
 
 export default function Home() {
   const router = useRouter()
   const [name, setName] = useState("Jasmin")
+  const [color, setColor] = useState("green")
 
   const handleData = () => {
     setName("Vraj")
   }
 
-  const navigate = (path)=>{
+  const navigate = (path) => {
     return router.push(path)
   }
   return (
     <>
       <main>
-        <User name={name} />
+        <User name={name} color={color}/>
         <h1 className={custom.main}>My Name Is {name}</h1>
         <Link href="/login" className={other.main}>Go to Link Page</Link>
         <br />
@@ -28,7 +29,7 @@ export default function Home() {
         <Link href="/productlist" className={other.main}>Go to Product List Page</Link>
         <br />
         <br />
-        <Link href="/userlist" className={other.main}>Go to User List Page</Link>
+        <Link href="/userlist" >Go to User List Page</Link>
         <br />
         <br />
         <Link href="/about" className={outside.main}>Go to About Page</Link>
@@ -40,16 +41,19 @@ export default function Home() {
         <button onClick={() => navigate("/about")}>Go to About Page</button>
         <br />
         <br />
+        <button onClick={() => setColor("orange")}>Change Color</button>
+        <br />
+        <br />
         <button onClick={handleData}>Click Here</button>
       </main>
     </>
   )
 }
 
-const User = (props) => {
+const User = ({ color, name }) => {
   return (
     <div>
-      <h1>Hello my name is {props.name}</h1>
+      <h1 className={color === "green" ? custom.main : other.main}>Hello my name is {name}</h1>
     </div>
   )
 }
