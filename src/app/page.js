699ec0e51a606bc195ceb6df051babfa861f568e
Jasmin2208 <jasminkorat204@gmail.com>
2023-08-31 14:38:45 +0530

    Check app working on production mode or development mode

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,15 @@ import { useState } from 'react'
 import Link from 'next/link'
 
 export default function Home() {
-
   return (
     <>
       <main>
         <h1 className='heading'>Home Page</h1>
+        {
+          process.env.NODE_ENV == 'development' ?
+            <h1>Development Mode</h1> :
+            <h1>Production Mode</h1>
+        }
       </main>
     </>
   )
