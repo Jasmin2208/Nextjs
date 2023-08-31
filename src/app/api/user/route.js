b1300b8bbc78fd3ea66d9dcb2e62b74d8794de5f
Jasmin2208 <jasminkorat204@gmail.com>
2023-08-31 18:10:58 +0530

    Make API with Post Method

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -5,3 +5,11 @@ export async function GET(request) {
     const data = users;
     return NextResponse.json(data)
 }
+
+export async function POST(request) {
+    const payload = await request.json()
+    if (!payload.name || !payload.age || !payload.gender) {
+        return NextResponse.json({ result: "Required Feild Not Found !!" })
+    }
+    return NextResponse.json({ result: "hello", data: payload })
+}
