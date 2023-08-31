35a2e4b4af15227cce9c1a8243108896a8722a54
Jasmin2208 <jasminkorat204@gmail.com>
2023-08-31 16:31:59 +0530

    Remove unwanted files

--- services/getUsers.js
+++
@@ -1,6 +0,0 @@
-async function getUsers() {
-    const data = await fetch("https://dummyjson.com/users")
-    return data.json()
-}
-
-export default getUsers;
--- src/app/config/constant.js
+++
@@ -1,1 +0,0 @@
-export const API_BASE_URL =  process.env.NODE_ENV == 'development' ? "localhost:3000" : "www.api.production.com"
