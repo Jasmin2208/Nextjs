f98e4f94fc3f35fa684e7dc78497b5df42081957
Jasmin2208 <jasminkorat204@gmail.com>
2023-08-31 18:48:58 +0530

    Make PUT API Next js

--- src/app/api/user/[id]/route.js
+++ src/app/api/user/[id]/route.js
@@ -5,3 +5,12 @@ export async function GET(request, content) {
     const userData = users.filter((items) => items.id == content.params.id)
     return NextResponse.json(userData.length === 0 ? { result: "No data Found !" } : userData)
 }
+
+export async function PUT(request, content) {
+    let payload = await request.json()
+    payload.id = content.params.id;
+    if (!payload.id || !payload.name || !payload.age || !payload.gender) {
+        return NextResponse.json({ result: "Required Feild Not Found !!", error: true })
+    }
+    return NextResponse.json({ result: "User Update Successfully !", data: payload, error: false })
+}
