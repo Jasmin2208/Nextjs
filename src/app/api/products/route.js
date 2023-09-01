94056af5061d43137e60ecc2ef62a83c9de00f2b
Jasmin2208 <jasminkorat204@gmail.com>
2023-09-01 11:54:00 +0530

    Make POST API with MongoDB in Nextjs

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -3,10 +3,21 @@ import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 import { Product } from "../../../utils/Models/productModel";
 
-export async function GET(request, content) {
+export async function GET() {
     try {
         await mongoose.connect(connectionToDB)
         const data = await Product.find()
+        return NextResponse.json({ result: data, error: false })
+    } catch (error) {
+        return NextResponse.json({ result: error.message, error: true })
+    }
+}
+
+export async function POST(request) {
+    try {
+        const payload = await request.json()
+        await mongoose.connect(connectionToDB)
+        const data = await new Product(payload).save()
         return NextResponse.json({ result: data, error: false })
     } catch (error) {
         return NextResponse.json({ result: error.message, error: true })
